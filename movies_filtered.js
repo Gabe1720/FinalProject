e841872354fbfb08ae22c3f166ddd06ffa92d815
Gabe1720 <hardyfgt@miamioh.edu>
2023-11-15 19:58:26 -0500

    Updated movies_filtered
    genre, rating, and platform should all filter as intended. The page also now gives the movies names as a url so you can access the movie page for that movie.

--- movies_filtered.js
+++ movies_filtered.js
@@ -1,54 +1,90 @@
-let allMovies = [];
+var allMovies = [];
 
-// Load CSV data from an external file
-fetch('movies_metadata.csv')
-    .then(response => response.text())
-    .then(data => parseCSV(data))
-    .catch(error => console.error('Error:', error));
-
-function parseCSV(csvText) {
-    Papa.parse(csvText, {
-        header: true,
-        skipEmptyLines: true,
-        complete: function(results) {
-            allMovies = results.data;
-            displaySortedMovieTitles(allMovies);
-        },
-        error: function(error) {
-            console.error('Error:', error);
-        }
-    });
-}
+        fetch("movies_metadata.csv")
+            .then(response => response.text())
+            .then(data => {
+                // Parse CSV data
+                const rows = data.split('\n');
+                const headers = rows[0].split(',');
+                // console.log(headers[22]);
+                headers[22] = "Disney";
+                for (let i = 1; i < rows.length; i++) {
+                    const rowData = rows[i].split(',');
+                    const movie = {};
+                    for (let j = 0; j < headers.length; j++) {
+                        movie[headers[j]] = rowData[j];
+                    }
+                    movie[headers[3]] = i - 1;
+                    let disney = movie.Disney + "";
+                    if (disney.includes('T')) {
+                        movie.Disney = 'TRUE';
+                    } else {
+                        movie.Disney = 'FALSE';
+                    }
+                    allMovies.push(movie);
+                    // console.log(movie);
+                }
+                displaySortedMovies(allMovies);
+            })
+            .catch(error => console.error('Error:', error));
+            
 
 function filterMovies() {
+    let myFilteredMovies = [];
     const genreInput = document.getElementById('genre').value.toLowerCase();
     const ratingInput = document.getElementById('rating').value.toLowerCase();
     const platformInput = document.getElementById('platform').value.toLowerCase();
 
-    const filteredMovies = allMovies.filter(movie =>
-        (!genreInput || movie.genres.toLowerCase().includes(genreInput)) &&
-        (!ratingInput || movie.vote_average.toString().toLowerCase().includes(ratingInput)) &&
-        (!platformInput || movie.production_companies.toLowerCase().includes(platformInput))
-    );
+    allMovies.forEach(movie => {
+        //console.log(movie.genres);
+        let genresStr = movie.genres + "";
+        let genreMatches = genresStr.toLowerCase().includes(genreInput) || !genreInput;
+        let ratingStr = movie.vote_average + "";
+        let ratingMatches = ratingStr.toLowerCase().includes(ratingInput) || !ratingInput;
+        let platformMatches = !platformInput;
 
-    displaySortedMovieTitles(filteredMovies);
+        if ((platformInput.includes('prime') || platformInput.includes('amazon')) && movie.Prime == 'TRUE') {
+            platformMatches = true;
+            //console.log("Streams on Prime");
+        }
+        if (platformInput.includes('netflix') && movie.Netflix == 'TRUE') {
+            platformMatches = true;
+            //console.log("Streams on Netflix");
+        }
+        if ((platformInput.includes('hbo') || platformInput.includes('max')) && movie.HBO_Max == 'TRUE') {
+            platformMatches = true;
+            //console.log("Streams on HBO Max");
+        }
+        if (platformInput.includes('disney') && movie.Disney == 'TRUE') {
+            platformMatches = true;
+            //console.log("Streams on Disney");
+        }
+
+        if (genreMatches && ratingMatches && platformMatches) {
+            myFilteredMovies.push(movie);
+        }
+    });
+
+    displaySortedMovies(myFilteredMovies);
 }
 
 function resetFilter() {
-    displaySortedMovieTitles(allMovies);
+    document.getElementById('genre').value = "";
+    document.getElementById('rating').value = "";
+    document.getElementById('platform').value = "";
+    displaySortedMovies(allMovies);
 }
 
-function displaySortedMovieTitles(movies) {
+function displaySortedMovies(movies) {
     const movieList = document.getElementById('movie-list');
     movieList.innerHTML = '';  // Clear previous results
 
-    // Extract and sort movie titles alphabetically
-    const sortedTitles = movies.map(movie => movie.title).sort();
-
-    // Display sorted titles
-    sortedTitles.forEach(title => {
+    // Display sorted movies
+    movies.forEach(movie => {
         const titleItem = document.createElement('li');
-        titleItem.textContent = title;
+        titleItem.innerHTML = `
+                <a href="./details.html?id=${movie.id}">${movie.title}</a>
+            `;
         movieList.appendChild(titleItem);
     });
 }
