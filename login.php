9eff1478e3460a50ed31b0e592d644980b05a14e
Gabe1720 <hardyfgt@miamioh.edu>
2023-12-02 16:33:58 -0500

    Integrated Login w/ Signup, touched up login, small details.html fix
    login should not work with the accounts database and both signup and login work as intended fully. Fixed small typo with details.html that messed up the popcorn image/made it not show.

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Get form data
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    // Create or open the CSV file for reading and writing
+    $csvFile = fopen("./Database_files/accounts/accounts.csv", "a+"); // "a" for append mode
+
+    // Check if the combination already exists
+    $valid = false;
+    while ($row = fgetcsv($csvFile)) {
+        if ($email === $row[0] && $password === $row[1]) {
+            $valid = true;
+            break;
+        }
+    }
+
+    // Close the CSV file
+    fclose($csvFile);
+
+    // Redirect or display a success message
+    if ($valid) {
+        header("Location: index.html");
+        exit();
+    } else {
+        header("Location: invalid.html");
+        exit();
+    }
+
+    exit();
+}
+?>
