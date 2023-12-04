c26212ab73d89de78c874d59f6420b9accfe0adb
caitlinspyra <113064305+caitlinspyra@users.noreply.github.com>
2023-12-03 20:11:12 -0500

    Create comments.js
    separated original comment.html into comment.html and comment.js

---
+++ comments.js
@@ -0,0 +1,27 @@
+function addComment() {
+    var commentInput = document.getElementById('comment-input');
+    var commentText = commentInput.value;
+
+    if (commentText.trim() !== '') {
+        var commentContainer = document.getElementById('previous-comments');
+        var noCommentsMessage = document.getElementById('no-comments-message');
+
+        // Check if the "no comments" message is visible and hide it
+        if (noCommentsMessage && noCommentsMessage.style.display !== 'none') {
+            noCommentsMessage.style.display = 'none';
+        }
+
+        // Get the current time
+        var currentTime = new Date();
+        var timestamp = currentTime.toLocaleString();
+        // Create a new comment element
+        var newComment = document.createElement('div');
+        newComment.className = 'comment';
+        newComment.innerHTML = '<span class="username">Anonymous:</span> ' + commentText +                                '<span class="timestamp"> (' + timestamp + ')</span>';
+        // Append the new comment to the comments container
+        commentContainer.appendChild(newComment);
+
+        // Clear the comment input field
+        commentInput.value = '';
+    }
+}
